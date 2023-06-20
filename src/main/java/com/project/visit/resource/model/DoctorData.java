fce60ed28603d09d2606f569ae3b5e4f6afadf3d
a.izadi <a.izadi@tapsell.ir>
2023-06-21 00:36:07 +0330

    delete id

--- src/main/java/com/project/visit/resource/model/DoctorData.java
+++ src/main/java/com/project/visit/resource/model/DoctorData.java
@@ -2,6 +2,6 @@ package com.project.visit.resource.model;
 
 import java.util.List;
 
-public record DoctorData(Long id, String name, String family, String medicalCode, String picLink, List<String> tags,
-						 List<AddressData> addressData) {
+public record DoctorData(String name, String family, String medicalCode, String picLink, List<String> tags,
+                         List<AddressData> addressData) {
 }
