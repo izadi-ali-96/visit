81de94431997b1356ede86b7a64fe863bfb84951
a.izadi <a.izadi@tapsell.ir>
2023-06-27 03:12:26 +0330

    fix

--- src/main/java/com/project/visit/resource/request/AssignRequestModel.java
+++ src/main/java/com/project/visit/resource/request/AssignRequestModel.java
@@ -1,6 +1,7 @@
 package com.project.visit.resource.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Null;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,6 +9,6 @@ import lombok.Setter;
 @Setter
 public class AssignRequestModel {
 
-	@NotBlank
+	@Null
 	private Long visitId;
 }
