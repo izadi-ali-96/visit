1d72e0e3993ae745deb96476f85d42582e9c5bd0
a.izadi <a.izadi@tapsell.ir>
2023-06-27 03:56:02 +0330

    pom

--- src/main/java/com/project/visit/model/Visit.java
+++ src/main/java/com/project/visit/model/Visit.java
@@ -13,7 +13,7 @@ public class Visit implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
 
     private Long time;
 
